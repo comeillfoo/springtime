9a1de6c9d6413fa598847478f33e49f81a70add2
Come1LLF00 <rayveleaveme34@inbox.ru>
2020-11-19 19:08:47 +0300

    initialize project in intellij IDE

---
+++ src/main/java/org/comrades/springtime/SpringtimeApplication.java
@@ -0,0 +1,13 @@
+package org.comrades.springtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/comrades/springtime/SpringtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.comrades.springtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
