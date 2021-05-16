57a9a75fca2976f238a665d0cf9badaa75e0be3a
come_ill_foo <rayveleaveme34@inbox.ru>
2021-05-16 12:22:46 +0300

    feat: added sending notifications

--- src/main/java/org/comrades/springtime/mbeans/DotsCounterMBean.java
+++ src/main/java/org/comrades/springtime/mbeans/DotsCounterMBean.java
@@ -9,18 +9,23 @@ import org.comrades.springtime.servise.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.jmx.export.notification.NotificationPublisher;
+import org.springframework.jmx.export.notification.NotificationPublisherAware;
 import org.springframework.stereotype.Component;
 
+import javax.management.Notification;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ManagedResource(
     objectName="PD:category=MBeans, name=dotsCounterMBean",
     description="Managed Bean for counting dots that out of area and dots at all"
 )
 @Component( "dotsCounterMBean" )
-public class DotsCounterMBean {
+public class DotsCounterMBean implements NotificationPublisherAware {
   private DotService dotService;
   private UserService userService;
+  private NotificationPublisher notificationPublisher;
 
   @Autowired
   public DotsCounterMBean( DotService dotService, UserService userService ) {
@@ -39,9 +44,27 @@ public class DotsCounterMBean {
       return new Pair<>( 0L, 0L );
 
     final Long total = ( long ) dots.size();
-    // TODO: write notifications
-    final Long notHit = dots.parallelStream().filter( ( dot )->( !dot.getHit() ) ).count();
+
+    // get the not hit dots
+    List<Dot> notHitDots = dots.parallelStream().filter( ( dot )->( !dot.getHit() ) ).collect( Collectors.toList());
+    final Long notHit = ( long ) notHitDots.size();
+
+    // send notifications about every dot
+    notHitDots.forEach( ( dot )->{
+      notificationPublisher.sendNotification(
+          new Notification(
+              "count",
+              this,
+              System.currentTimeMillis(),
+              String.format( "точка ( %f %f ) за пределами области[ R = %d ]", dot.getX(), dot.getY(), dot.getR() ) )
+      );
+    } );
 
     return new Pair<>( total, notHit );
+  }
+
+  @Override
+  public void setNotificationPublisher( NotificationPublisher notificationPublisher ) {
+    this.notificationPublisher = notificationPublisher;
   }
 }
